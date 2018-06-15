10803083741d9c468419d5aa302a325e6514b3dd
s0i37 <s0i37@work>
2018-06-15 10:11:20 +0500

    added web-gui

---
+++ search/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+var server = require("./server")
+var router = require("./router")
+var requestHandlers = require("./requestHandlers")
+
+var handle = {}
+handle[""] = requestHandlers.start
+handle["search"] = requestHandlers.search
+handle["auto"] = requestHandlers.autocomplete
+handle["cache"] = requestHandlers.cache
+
+server.start( router.route, handle )
